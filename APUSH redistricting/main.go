11bec70756110222adf409775864733ebe320d69
crimsonrosen <77373859+crimsonrosen@users.noreply.github.com>
2021-06-13 16:05:10 -0700

    Update main.go
    changed maxelectors flag to getmax

--- APUSH redistricting/main.go
+++ APUSH redistricting/main.go
@@ -6,7 +6,7 @@ import (
 	functions "./functions"
 	)
 func main() {
-	purpose := flag.String("purpose", "test", "test/redistrict/maxelectors function")
+	purpose := flag.String("purpose", "test", "test/redistrict/getmax function")
 	electCount := flag.Int("count", 538, "total number of electors")
 	senators := flag.Int("senators", 2, "how many senators per state")
 	flag.Parse() 
@@ -16,7 +16,7 @@ func main() {
 		functions.Test(List, float64(*electCount))
 	} else if *purpose == "redistrict" {
 		functions.Redistrict(List, float64(*electCount))
-	} else if *purpose == "maxelectors" {
+	} else if *purpose == "getmax" {
 		functions.Max(List, *senators)
 	}
 	
